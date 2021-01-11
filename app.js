e161fe6880d8b4fa8900682991bddef25134cce6
Vlad <vlad10839@gmail.com>
2021-01-11 15:16:18 +0200

    Ekz

---
+++ app.js
@@ -0,0 +1,82 @@
+(function () {
+    'use strict';
+
+    var factory2 = new ShoppingListService();
+
+    angular.module('ControllerAsApp', [])
+        .controller('ShoppingListController1', ShoppingListController1)
+        .factory('ShoppingListFactory', ShoppingListFactory);
+
+    ShoppingListController1.$inject = ['ShoppingListFactory'];
+
+    function ShoppingListController1(ShoppingListFactory) {
+        var list1 = this;
+
+        var shoppingList = ShoppingListFactory();
+
+        list1.item = shoppingList.getItems();
+
+        list1.removeItem = function (index) {
+            shoppingList.removeItem(index);
+        };
+        list1.hide = function(index){
+            shoppingList.hide(index);
+        }
+    }
+
+    function ShoppingListService() {
+        var service = this;
+
+        var items = [
+        {
+                mark: "МОлоко",
+                quantity: 124241,
+                show : true
+            },
+            {
+                mark: "Гарний йогурт",
+                quantity: 123213,
+                 show : true
+            },
+            {
+                mark: "Найкращий",
+                quantity: 12,
+                 show : true
+            },
+            {
+                mark: "Мій",
+                quantity: 32,
+                 show : true
+            },
+            {
+                mark: "Файний",
+                quantity: 4,
+                 show : true
+            },
+            {
+                mark: "РРАААПАП",
+                quantity: 333,
+                 show : true
+            },
+        ];
+
+        service.removeItem = function (itemIndex) {
+            items.splice(itemIndex, 1);
+        };
+
+        service.getItems = function () {
+            return items;
+        };
+            service.hide = function(index)
+        {
+             items[index].show = false;
+        }
+    }
+
+    function ShoppingListFactory() {
+        var factory = function () {
+            return factory2;
+        };
+        return factory;
+    }
+})();
